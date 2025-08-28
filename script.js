5739f65af72b7d7f0d3ec57968cb3c4128a61ffe
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2025-08-28 11:05:58 +0100

    fix tab bug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let preTipTotal = 150;
-let tipPercentage = 7;
+let preTipTotal = 130;
+let tipPercentage = 12;
 let tip = preTipTotal / 100 * tipPercentage;
 tip = Number(tip.toFixed(2))
 let total = preTipTotal + tip; 
@@ -7,6 +7,6 @@ let total = preTipTotal + tip;
 console.log("TIP CALCULATOR:");
 console.log(`Cost of the meal:\t ${preTipTotal.toFixed(2)}£`);
 console.log(`${tipPercentage}%`);
-console.log(`Tip amount:\t\t\t ${tip.toFixed(2)}£`);
+console.log(`Tip amount:\t\t ${tip.toFixed(2)}£`);
 console.log(`Total amount:\t\t ${total.toFixed(2)}£`);
 
